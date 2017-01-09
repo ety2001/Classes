2be53a1bdf09f4ccd62b533a74195ef1843a7660
ety2001 <elaine.t.yang1023@gmail.com>
2017-01-09 00:23:19 -0800

    Update movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -1,5 +1,5 @@
 //movie.cpp
-
+//implements the movie class
 #include "movie.h"
 
 movie::movie(int movieYear, char* movieTitle, char* movieDirector, int movieDuration, int movieRating) : media(3, movieYear, movieTitle) {
